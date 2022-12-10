487c7a0126f96efc08a2d1451a78aa567f5c3e9c
Luca Liebenberg <liebenbergluca@gmail.com>
2022-12-10 13:28:33 +0200

    Initial commit

---
+++ components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Welcome to the home page!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
